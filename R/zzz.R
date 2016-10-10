711d4541fef9ce90c18f218061986dae1f20aadf
John Coene <jcoenep@gmail.com>
2016-10-10 18:04:25 +0800

    setup env renamed

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
-url_env <- new.env(hash = TRUE)
+setup_env <- new.env(hash = TRUE)
 
 .onAttach <- function(libname = find.package("rindeed"), pkgname = "rindeed") {
   packageStartupMessage("help('rindeed') for examples")
