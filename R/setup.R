5e02e18f9969d67be119c9482432d1ee6c7ef42a
John Coene <jcoenep@gmail.com>
2016-10-13 18:32:16 +0800

    moved setting FUn to setup file

--- R/setup.R
+++ R/setup.R
@@ -64,3 +64,30 @@ in_setup <- function(base.url = NULL, country = NULL){
   assign("base.url", df$base.url, envir = setup_env)
   assign("country", df$country, envir = setup_env)
 }
+
+#' Retrieve settings
+#' 
+#' Rerieve settings set with \code{\link{in_setup}}
+#' 
+#' @return Returns data.frame of settings:
+#' \itemize{
+#'  \item base.url
+#'  \item country
+#' }
+#' 
+#' @examples
+#' in_setup(country = "us") 
+#' (my_settings <- in_settings())
+#' 
+#' @export
+in_settings <- function(){
+  base.url <- tryCatch({get("base.url", setup_env)},
+                       error = function(e) e)
+  if(is(base.url, "error")) stop("no settings")
+  
+  country <- tryCatch({get("country", setup_env)},
+                      error = function(e) e)
+  set = data.frame(base.url = base.url,
+                   country = country)
+  return(set)
+}
